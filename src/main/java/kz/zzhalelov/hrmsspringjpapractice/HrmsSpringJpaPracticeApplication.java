420af2d7ce32efd0f2541304d0eaafdf529a1b21
Zhastilek Zhalelov <zzhalelov@gmail.com>
2025-02-17 22:51:58 +0500

    git init

---
+++ src/main/java/kz/zzhalelov/hrmsspringjpapractice/HrmsSpringJpaPracticeApplication.java
@@ -0,0 +1,13 @@
+package kz.zzhalelov.hrmsspringjpapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsSpringJpaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrmsSpringJpaPracticeApplication.class, args);
+    }
+
+}
