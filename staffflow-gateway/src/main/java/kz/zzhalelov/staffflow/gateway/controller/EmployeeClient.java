9cc2baf001567de2cc3f52a8eb28592427ad7353
Zhastilek Zhalelov <zzhalelov@gmail.com>
2025-06-30 22:52:58 +0500

    refactoring: add delete method in gateway

--- staffflow-gateway/src/main/java/kz/zzhalelov/staffflow/gateway/controller/EmployeeClient.java
+++ staffflow-gateway/src/main/java/kz/zzhalelov/staffflow/gateway/controller/EmployeeClient.java
@@ -3,10 +3,13 @@ package kz.zzhalelov.staffflow.gateway.controller;
 import kz.zzhalelov.staffflow.server.dto.employeeDto.EmployeeCreateDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import java.util.Map;
 
 @Component
 public class EmployeeClient {
@@ -21,5 +24,17 @@ public class EmployeeClient {
 
     public ResponseEntity<Object> create(EmployeeCreateDto employeeCreateDto) {
         return restTemplate.postForEntity("/api/employees", employeeCreateDto, Object.class);
+    }
+
+    public ResponseEntity<Object> findAll() {
+        return restTemplate.getForEntity("/api/employees", Object.class);
+    }
+
+    public ResponseEntity<Object> delete(long emoloyeeId) {
+        return restTemplate.exchange("/api/employees/{employeeId}",
+                HttpMethod.DELETE,
+                null,
+                Object.class,
+                Map.of("employeeId", emoloyeeId));
     }
 }
--- staffflow-gateway/src/main/java/kz/zzhalelov/staffflow/gateway/controller/EmployeeController.java
+++ staffflow-gateway/src/main/java/kz/zzhalelov/staffflow/gateway/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import jakarta.validation.Valid;
 import kz.zzhalelov.staffflow.server.dto.employeeDto.EmployeeCreateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -18,5 +15,15 @@ public class EmployeeController {
     @PostMapping()
     public ResponseEntity<Object> create(@Valid @RequestBody EmployeeCreateDto employeeCreateDto) {
         return employeeClient.create(employeeCreateDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> findAll() {
+        return employeeClient.findAll();
+    }
+
+    @DeleteMapping("/{employeeId}")
+    public ResponseEntity<Object> deleteById(@PathVariable long employeeId) {
+        return employeeClient.delete(employeeId);
     }
 }
