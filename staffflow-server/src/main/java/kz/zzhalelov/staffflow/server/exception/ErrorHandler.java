e7d36a167ab3d62c5087e8637a35727ef5c60829
Zhastilek Zhalelov <zzhalelov@gmail.com>
2025-07-01 23:47:42 +0500

    feat: add ErrorHandler & ErrorResponse

---
+++ staffflow-server/src/main/java/kz/zzhalelov/staffflow/server/exception/ErrorHandler.java
@@ -0,0 +1,19 @@
+package kz.zzhalelov.staffflow.server.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorHandler {
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFound(NotFoundException ex) {
+        log.warn(ex.getMessage());
+        return new ErrorResponse(ex.getMessage());
+    }
+}
---
+++ staffflow-server/src/main/java/kz/zzhalelov/staffflow/server/exception/ErrorResponse.java
@@ -0,0 +1,5 @@
+package kz.zzhalelov.staffflow.server.exception;
+
+public record ErrorResponse(String error) {
+
+}
