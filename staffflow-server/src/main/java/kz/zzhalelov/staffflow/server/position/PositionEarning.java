bda45367f10eb5cf6a9a02dcb82be79291c67dc2
Zhastilek Zhalelov <zzhalelov@gmail.com>
2025-10-11 22:57:20 +0500

    fix: add @ManyToOne(fetch = FetchType.LAZY) in PositionEarning

--- staffflow-server/src/main/java/kz/zzhalelov/staffflow/server/position/PositionEarning.java
+++ staffflow-server/src/main/java/kz/zzhalelov/staffflow/server/position/PositionEarning.java
@@ -3,7 +3,6 @@ package kz.zzhalelov.staffflow.server.position;
 import jakarta.persistence.*;
 import kz.zzhalelov.staffflow.server.earning.EarningType;
 import lombok.AccessLevel;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
@@ -18,11 +17,12 @@ public class PositionEarning {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "earning_type_id", nullable = false)
     EarningType earningType;
 
     Double amount;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "position_id")
     Position position;
-
 }
