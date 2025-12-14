c828917024adeb2d05e8a255adfacac6f1c57729
Zhastilek Zhalelov <zzhalelov@gmail.com>
2025-12-14 18:33:15 +0500

    test: fix Department, Employee, RegulatoryIndicator entities tests

--- staffflow-server/src/main/java/kz/zzhalelov/staffflow/security/SecurityConfig.java
+++ staffflow-server/src/main/java/kz/zzhalelov/staffflow/security/SecurityConfig.java
@@ -3,35 +3,43 @@
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.core.userdetails.User;
 //import org.springframework.security.core.userdetails.UserDetails;
 //import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 //import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 //import org.springframework.security.web.SecurityFilterChain;
 //
 //@Configuration
+//@EnableWebSecurity
 //public class SecurityConfig {
-//
-//    // Пользователь "admin" с паролем "secret123"
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user = User.withUsername("admin")
-//                .password("{noop}secret123") // {noop} значит "без шифрования"
-//                .roles("ADMIN")
-//                .build();
-//        return new InMemoryUserDetailsManager(user);
-//    }
-//
-//    // Настройка HTTP безопасности
 //    @Bean
 //    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        http
-//                .csrf().disable() // для Postman и API можно отключить CSRF
 //                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/**").authenticated() // защищаем только API
-//                        .anyRequest().permitAll()
-//                )
-//                .httpBasic(); // Basic Auth
+//                        .requestMatchers("/**").permitAll()
+//                        .requestMatchers("/**").hasRole("ADMIN")
+//                        .anyRequest().authenticated()
+//                );
+//
 //        return http.build();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin123"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin);
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
 //    }
 //}
