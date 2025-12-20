340024ee4ceb5ca31a4405df194aa0b97ee20962
Zhastilek Zhalelov <zzhalelov@gmail.com>
2025-12-20 15:03:29 +0500

    fix: set CORS

--- staffflow-server/src/main/java/kz/zzhalelov/staffflow/server/config/WebConfig.java
+++ staffflow-server/src/main/java/kz/zzhalelov/staffflow/server/config/WebConfig.java
@@ -1,6 +1,7 @@
-package kz.zzhalelov.staffflow.config;
+package kz.zzhalelov.staffflow.server.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,15 +12,15 @@ public class WebConfig {
     @Value("${cors.allowed-origins}")
     private String[] allowedOrigins;
 
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins(allowedOrigins)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true)
                         .maxAge(3600);
             }
         };
