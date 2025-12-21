4f04043b0d47c7a5097dec72ae315cfca89734e5
Zhastilek Zhalelov <zzhalelov@gmail.com>
2025-12-21 20:01:15 +0500

    fix: Prometheus updating error, fix WebConfig

--- staffflow-server/src/main/java/kz/zzhalelov/staffflow/server/config/WebConfig.java
+++ staffflow-server/src/main/java/kz/zzhalelov/staffflow/server/config/WebConfig.java
@@ -1,16 +1,23 @@
 package kz.zzhalelov.staffflow.server.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@ConfigurationProperties(prefix = "cors")
 public class WebConfig {
 
-    @Value("${cors.allowed-origins}")
-    private String[] allowedOrigins;
+    private List<String> allowedOrigins;
+
+    public void setAllowedOrigins(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -18,7 +25,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins(allowedOrigins)
+                        .allowedOrigins(allowedOrigins.toArray(new String[0]))
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .maxAge(3600);
