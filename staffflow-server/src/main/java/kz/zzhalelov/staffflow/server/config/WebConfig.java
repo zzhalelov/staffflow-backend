2105b342b5b4fb0ee064b5e0307c3d18988b3581
Zhastilek Zhalelov <zzhalelov@gmail.com>
2026-02-25 21:55:44 +0500

    fix: bug with swagger page opening

--- staffflow-server/src/main/java/kz/zzhalelov/staffflow/server/config/WebConfig.java
+++ staffflow-server/src/main/java/kz/zzhalelov/staffflow/server/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -28,6 +29,15 @@ public class WebConfig {
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .maxAge(3600);
+            }
+
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/swagger-ui/**")
+                        .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/");
+
+                registry.addResourceHandler("/**")
+                        .addResourceLocations("classpath:/static/");
             }
         };
     }
